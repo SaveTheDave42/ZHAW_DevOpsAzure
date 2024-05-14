6e0e189f67a105e6773fb5f62e792a5e98be3657
DavidVocat <david.rappl@gmail.com>
2024-05-14 15:53:11 +0200

    new text change for github actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to the DevOs Docker Test from David Vocat. Github Actions work now');
+  res.send('Welcome to the DevOs Docker Test from David Vocat. Github Actions work');
 });
 
 app.listen(PORT, HOST);
